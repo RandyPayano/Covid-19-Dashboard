7a8f13abc7a77a7a76c409eb8721d8dff4618ae7
Randy P <randypayano@gmail.com>
2020-03-21 13:49:11 -0400

    finished Dashboard

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from bson.json_util import dumps
 import bson.json_util as json_util
 import quandl
 import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__, static_url_path='')
 
@@ -41,7 +42,6 @@ final_table['Cases Recovered'] =  final_table['TotalRecovered'] / final_table['T
 final_table['Cases Active'] =  final_table['ActiveCases'] / final_table['TotalCases'] * 100
 final_table['Mortality Rate'] =  final_table['TotalDeaths'] / final_table['TotalCases'] * 100
 
-print(final_table)
 
 
 
@@ -57,14 +57,14 @@ sorted_totals  =sorted_totals[['Country','TotalCases','TotalDeaths','ActiveCases
 sorted_totals  = sorted_totals.rename(columns={"TotalCases": "Cases","TotalDeaths": "Deaths", "ActiveCases": "Active"})
 sorted_totals = sorted_totals.set_index('Country')
 
-print(sorted_totals)
+
 sorted_popaffectcsv = final_table.sort_values(by=['PopulationAffected'], ascending=False)
-sorted_popaffectcsv = sorted_popaffectcsv.iloc[:15,:]
+sorted_popaffectcsv = sorted_popaffectcsv.iloc[:25,:]
 sorted_popaffectcsv.to_csv('static/images/sorted_popaffectcsv.csv')
 
-sorted_mortalityratecsv = final_table.sort_values(by=['Mortality Rate'], ascending=False)
+sorted_mortalityratecsv = final_table.iloc[:-1].sort_values(by=['Mortality Rate'], ascending=False)
 sorted_mortalityratecsv = sorted_mortalityratecsv[sorted_mortalityratecsv["TotalCases"] > 20]
-sorted_mortalityratecsv = sorted_mortalityratecsv.iloc[:30,:]
+sorted_mortalityratecsv = sorted_mortalityratecsv.iloc[:25,:]
 sorted_mortalityratecsv = sorted_mortalityratecsv.rename(columns={"Mortality Rate": "MortalityRate"})
 sorted_mortalityratecsv.to_csv('static/images/sorted_mortalityratecsv.csv')
 
@@ -99,8 +99,10 @@ def do_lat_long():
         if e >= 8000 and e < 22000:    
             graphing_value.append(1300)
         if e >= 22000 and e < 35000:    
-            graphing_value.append(2000)    
-        if e >= 35000:    
+            graphing_value.append(1700)    
+        if e >= 35000 and e < 55000:    
+            graphing_value.append(2200)    
+        if e >= 55000:    
             graphing_value.append(3000)   
         
     lat_long['graphing_value'] = graphing_value
@@ -118,12 +120,45 @@ def do_lat_long():
 
 def dict_list():
     info_mongodbpairs = pd.read_csv('static/images/covid16_table.csv')
-    info_mongodbpairs = info_mongodbpairs.iloc[:,1:]
+    info_mongodbpairs = info_mongodbpairs.iloc[:,1:].fillna('0')
+    info_mongodbpairs = info_mongodbpairs.drop(columns=['lat', 'long'])
+
+    info_mongodbpairs =info_mongodbpairs.rename(columns={"TotalCases": "Total Cases", "PopulationAffected": "Population Affected", "NewCases": "New Cases","NewDeaths": "New Deaths", "TotalRecovered": "Total Recovered","TotalDeaths": "Total Deaths", "ActiveCases": "Active Cases"})
+    info_mongodbpairs['Population Affected'] = info_mongodbpairs['Population Affected'].round(3).astype(str) + '%'
+    info_mongodbpairs['Mortality Rate'] = info_mongodbpairs['Mortality Rate'].round(2).astype(str) + '%'
+    info_mongodbpairs['Cases Active'] = info_mongodbpairs['Cases Active'].round(2).astype(str) + '%'
+    info_mongodbpairs['Cases Recovered'] = info_mongodbpairs['Cases Recovered'].round(2).astype(str) + '%'
+    info_mongodbpairs['Population'] = (info_mongodbpairs['Population'] /1000000).round(1).astype(str) + 'M'
+
+    info_mongodbpairs =info_mongodbpairs.rename(columns={"Population": "A) World Population","Population Affected": "A) World Population Affected",  "Total Cases": "Aa) Total Cases",  "Total Recovered": "D) Total Recovered", "Cases Recovered": "D) Percentage Recovered","Total Deaths":"G) Total Deaths", "Mortality Rate": "G) Mortality Rate", "Active Cases": "E) Active Cases","Cases Active": "E) Percentage Active", "Critical": "F) Critical Condition", "Critical": "G) Critical Condition"})
+
+    info_mongodbpairs = info_mongodbpairs.round(3)
+    info_mongodbpairs = info_mongodbpairs.drop(columns=['New Cases', 'New Deaths'])
+    info_mongodbpairs =  info_mongodbpairs.sort_values(by="Country")
     list_of_dicts = []
     info_mongodbpairs = info_mongodbpairs.transpose() 
     info_mongodbpairs = info_mongodbpairs.to_dict()
     list_of_dics = [value for value in info_mongodbpairs.values()]
+
+    list_of_dics = sorted(list_of_dics, key=lambda k: k['Country']) 
+
     return list_of_dics
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route("/")
@@ -137,13 +172,18 @@ def home_page():
     covid16_table = covid16_table.fillna('0')
     covid16_table.rename(columns = {'Country,Other':'Country', 'Serious,Critical':'Critical'},inplace = True) 
     final_table = covid16_table
+
+
     startswithlist = []
- 
+
     for i in covid16_table.NewCases:
-        if str(i).startswith('+'):
-            startswithlist.append(i.replace(',',''))
+        if "," in str(i):
+            startswithlist.append(i[1:].replace(',', ''))
+            
         else:
-            startswithlist.append(i.replace(',',''))
+         
+            startswithlist.append(i)
+
 
     covid16_table['sortingnewcases'] = startswithlist
     covid16_table['sortingnewcases'] = covid16_table['sortingnewcases'].apply(pd.to_numeric)
@@ -152,13 +192,17 @@ def home_page():
 
     sorted_newcases = sorted_newcases.set_index('Country')
     sorted_newcases = sorted_newcases[['NewCases', 'NewDeaths']].fillna(0)
+    sorted_newcases = sorted_newcases.rename(columns={'NewCases':"Cases", "NewDeaths": "Deaths"})
     sorted_newcases = sorted_newcases.iloc[:,:]
+
+
+
+
 
     final_table = pd.read_csv('static/images/covid16_table.csv')
     final_table = final_table.iloc[:,1:]
     
 
-    
     #TotalCases
     totalcases = final_table.iloc[-1:,1:2]
     totalcases = totalcases.rename(columns={"TotalCases": "Confirmed Cases"})
@@ -195,9 +239,37 @@ def home_page():
     mortalityrate= final_table.iloc[-1:,15:16].round(2).astype(str) + '%'
     mortalityrate = mortalityrate.set_index('Mortality Rate')
    
+    # NEWSSSSSSSSSSSSSSSSSSSSSSSSSS
 
   
-    return render_template("index.html", sorted_totals =sorted_totals.to_html(), sorted_newcases = sorted_newcases.to_html(), totalcases = totalcases.to_html(), newdeaths = newdeaths.to_html(),newcases=newcases.to_html(), totaldeaths=totaldeaths.to_html(), totalrecovered = totalrecovered.to_html(), activecases = activecases.to_html(), popaffected = popaffected.to_html(), pctrecovered = pctrecovered.to_html(), pctactive = pctactive.to_html(), mortalityrate = mortalityrate.to_html())
+    page = requests.get('http://newsapi.org/v2/everything?q=Corona?Virus&from=2020-03-10&sortBy=publishedAt&apiKey=71788d9278894c70987a0a2d0e8c6120')
+    page = page.json()
+
+
+    articlesource = []
+    articletitle = []
+    articleimage = []
+    articlewhen = []
+    articleurl = []
+    for e in range(4):
+        articlesource.append(page['articles'][e]['source']['name'])
+        articletitle.append(page['articles'][e]['title'])
+        articleurl.append(page['articles'][e]['url'])
+        articleimage.append(page['articles'][e]['urlToImage'])
+        articlewhen.append(page['articles'][e]['publishedAt'])
+        
+    dictionary1= {'articlesource': articlesource[0], 'title': articletitle[0], 'img_url':articleurl[0], 'articleimage': articleimage[0], 'articlewhen':articlewhen[0] }
+    dictionary2= {'articlesource': articlesource[1], 'title': articletitle[1], 'img_url':articleurl[1], 'articleimage': articleimage[1], 'articlewhen':articlewhen[1] }    
+    dictionary3= {'articlesource': articlesource[2], 'title': articletitle[2], 'img_url':articleurl[2], 'articleimage': articleimage[2], 'articlewhen':articlewhen[2] }
+    dictionary4= {'articlesource': articlesource[3], 'title': articletitle[3], 'img_url':articleurl[2], 'articleimage': articleimage[3], 'articlewhen':articlewhen[3] }
+
+
+
+
+    mydictlisty = [dictionary1,dictionary2]
+
+  
+    return render_template("index.html", mydictlisty = mydictlisty, dictionary1 = dictionary1, dictionary2= dictionary2,dictionary3= dictionary3, dictionary4= dictionary4, sorted_totals =sorted_totals.to_html(), sorted_newcases = sorted_newcases.to_html(), totalcases = totalcases.to_html(), newdeaths = newdeaths.to_html(),newcases=newcases.to_html(), totaldeaths=totaldeaths.to_html(), totalrecovered = totalrecovered.to_html(), activecases = activecases.to_html(), popaffected = popaffected.to_html(), pctrecovered = pctrecovered.to_html(), pctactive = pctactive.to_html(), mortalityrate = mortalityrate.to_html())
 
 
 @app.route('/names')
@@ -224,7 +296,7 @@ def sample_metadata(sample = "China"):
     for dicte in dict_list():
         if dicte['Country'] == sample:
             dataDict = dicte
-        
+
     return jsonify(dataDict)
 
 
